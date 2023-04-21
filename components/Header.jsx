4fcbc73342e88a7552404749929f88dec469910d
Kaja <kathleengracenoble@gmail.com>
2023-04-21 09:49:07 -0400

    built out more of site with multiple levels of  navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,36 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <Link to="/about">About</Link>
+                <NavLink 
+                    to="/host"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="/about"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="/vans"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
             </nav>
         </header>
     )
 }
-{/* <Link to="/host">Host</Link> */}
-{/* <Link to="/vans">Vans</Link> */}
